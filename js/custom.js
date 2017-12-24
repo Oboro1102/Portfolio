217f21e66ae9d4216b60779e7eecd17bedac986e
Oboro1102 <34804436+Oboro1102@users.noreply.github.com>
2017-12-24 12:38:20 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    $(window).bind('scroll', function () {
+        var barH = $("#fixednavbar").height();
+        var navHeight = $(window).height() - barH;
+        if ($(window).scrollTop() > navHeight) {
+            $('#fixednavbar').addClass('fixed-top');
+        } else {
+            $('#fixednavbar').removeClass('fixed-top');
+        }
+    });
+});
+$('.navbar-brand').click(function (e) {
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop: $('#home').offset().top
+    }, 700);
+});
+$('#about-click').click(function (e) {
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop: $('#about').offset().top
+    }, 700);
+});
+$('#skill-click').click(function (e) {
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop: $('#skill').offset().top
+    }, 700);
+});
+$('#workcollection-click').click(function (e) {
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop: $('#workcollection').offset().top
+    }, 700);
+});
+$('#contact-click').click(function (e) {
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop: $('#contact').offset().top
+    }, 700);
+});
