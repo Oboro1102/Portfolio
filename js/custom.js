849f2e19893f80d1172414793149287c5d8b80c8
Oboro Kuo <34804436+Oboro1102@users.noreply.github.com>
2018-01-13 21:27:20 +0800

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -3,29 +3,32 @@ $('#home-click').click(function (e) {
     e.preventDefault();
     $('html,body').animate({
         scrollTop: $('#home').offset().top
-    }, 700);
+    }, 1000);
 });
 $('#about-click').click(function (e) {
+    var barH = $("#customenavbar").height();
     e.preventDefault();
     $('html,body').animate({
-        scrollTop: $('#about').offset().top
-    }, 700);
+        scrollTop: $('#about').offset().top - barH
+    }, 1000);
 });
 $('#experience-click').click(function (e) {
+    var barH = $("#customenavbar").height();
     e.preventDefault();
     $('html,body').animate({
-        scrollTop: $('#experience').offset().top
-    }, 700);
+        scrollTop: $('#experience').offset().top - barH
+    }, 1000);
 });
 $('#skill-click').click(function (e) {
+    var barH = $("#customenavbar").height();
     e.preventDefault();
     $('html,body').animate({
-        scrollTop: $('#skill').offset().top
-    }, 700);
+        scrollTop: $('#skill').offset().top - barH
+    }, 1000);
 });
 $('#workcollection-click').click(function (e) {
     e.preventDefault();
     $('html,body').animate({
         scrollTop: $('#workcollection').offset().top
-    }, 700);
+    }, 1000);
 });
