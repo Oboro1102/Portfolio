64ecf700b01639778841344fcf343e1bdf7fe75d
Oboro Kuo <34804436+Oboro1102@users.noreply.github.com>
2018-05-31 00:58:21 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -22,4 +22,40 @@ $(document).ready(function () {
     $(".overlay").on("click", function () {
         $("button#close-left-menu").click();
     });
+    /* 回到頂端動畫 */
+    $('#back-to-top').on('click', function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+    /*連結轉跳*/
+    $('#logo').click(function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+    $('#about-click').click(function () {
+        var barH = $("#navarea").height();
+        $('html,body').animate({
+            scrollTop: $('#about').offset().top - barH
+        }, 500);
+    });
+    $('#experience-click').click(function () {
+        var barH = $("#navarea").height();
+        $('html,body').animate({
+            scrollTop: $('#experience').offset().top - barH
+        }, 500);
+    });
+    $('#skill-click').click(function () {
+        var barH = $("#navarea").height();
+        $('html,body').animate({
+            scrollTop: $('#skill').offset().top - barH
+        }, 500);
+    });
+    $('#workcollection-click').click(function () {
+        var barH = $("#navarea").height();
+        $('html,body').animate({
+            scrollTop: $('#workcollection').offset().top - barH
+        }, 500);
+    });
 });
