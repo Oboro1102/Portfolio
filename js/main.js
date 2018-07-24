757c29bf800ae0133badd9243d137f1f98b65282
Oboro Kuo <34804436+Oboro1102@users.noreply.github.com>
2018-07-24 22:56:10 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -22,8 +22,7 @@ $(".overlay").on("click", function () {
     $("button#close-left-menu").click();
 });
 /* 回到頂端動畫 */
-$('#back-to-top').on('click', function (e) {
-    e.preventDefault();
+$('#back-to-top').on('click', function () {
     $('html,body').animate({
         scrollTop: 0
     }, 500);
