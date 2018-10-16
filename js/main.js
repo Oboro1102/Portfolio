4c4782300fda5ce29b54a471f5a6953e993e48c9
Oboro Kuo <34804436+Oboro1102@users.noreply.github.com>
2018-10-17 02:55:09 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,47 +3,42 @@ $(document).ready(function () {
     /*導覽列滑動Fixed*/
     $(window).bind("scroll load", function () {
         if ($(this).scrollTop() > 0) {
-            $('#navarea').addClass('show-animate');
-            $('#back-to-top').show("fade", 300);
+            $("#navarea").addClass("scroll");
+            $("#back_to_top").show("fade", 300);
         } else {
-            $('#navarea').removeClass('show-animate');
-            $('#back-to-top').hide();
+            $("#navarea").removeClass("scroll");
+            $("#back_to_top").hide("fade", 300);
+        };
+
+        var homeHeight = $("#home").outerHeight();
+        if ($(this).scrollTop() > homeHeight) {
+            $("#IntroZone").addClass("invisible");
+        } else {
+            $("#IntroZone").removeClass("invisible");
         };
     });
 
-    /* 遮罩&禁止視窗滾動 */
-    $(".navshow").on("click", function () {
-        $(".overlay").show();
-        $("body").addClass("windowband");
-    });
-    $(".navclose").on("click", function () {
-        $(".overlay").hide();
-        $("body").removeClass("windowband");
-    });
-
-    /* 遮罩關閉選單 */
-    $(".overlay").on("click", function () {
-        $("button#close-left-menu").click();
-    });
-
     /* 回到頂端動畫 */
-    $('#back-to-top,#logo').on('click', function () {
-        $('html,body').animate({
+    $("#back_to_top").click(function (e) {
+        e.preventDefault;
+        $("html,body").animate({
             scrollTop: 0
         }, 500);
     });
 
-    /*連結轉跳*/
-    $('.smooth_hash').on("click", function () {
-        if ($("#navarea").hasClass("show-animate")) {
-            $('html,body').animate({
-                scrollTop: $($(this).attr("href")).offset().top - 49
-            }, 500);
-        } else {
-            $('html,body').animate({
-                scrollTop: $($(this).attr("href")).offset().top - 119
-            }, 500);
-        };
+    /* 平滑轉跳 */
+    $(".smooth_hash").click(function (e) {
+        e.preventDefault;
+        var navHeight = $(".navbar_list").outerHeight();
+        var offsetHeight = $($(this).attr("href")).offset().top - navHeight;
+        $('html,body').animate({
+            scrollTop: offsetHeight
+        }, 500);
+    });
+
+    /* 經歷單一展開 */
+    $("#experience").bind("show.bs.collapse collapse", function () {
+        $(this).find(".collapse.show").collapse('hide');
     });
 
 });
